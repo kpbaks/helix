6d5491be239c453584dbf26a73ad669148f6b6c0
kpbaks <kristoffer.pbs@tuta.io>
2026-01-31 22:17:12 +0100

    WIP! queries

---
+++ runtime/queries/pre-commit-config/tags.scm
@@ -0,0 +1,18 @@
+; select hooks and repos
+; ; select jobs
+; (block_mapping
+;   (block_mapping_pair
+;     value: (block_node
+;              (block_mapping
+;                (block_mapping_pair
+;                  key: (flow_node) @_key (#eq? @_key "stage"))))) @definition.struct)
+
+; ; select defined variables under `variables:`
+; (block_mapping
+;   (block_mapping_pair
+;     key: (flow_node) @_key (#eq? @_key "variables")
+;     value: (block_node
+;              (block_mapping
+;                (block_mapping_pair
+;                  key: (flow_node) @name
+;                  value: (_) @definition.constant)))))
